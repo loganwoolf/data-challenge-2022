60e0718c62615279b9edf915aa1e3a50ff3138a2
Logan Woolf <loganwoolf@gmail.com>
2022-04-15 21:27:17 -0600

    Complete challenge 5

---
+++ 05.py
@@ -0,0 +1,21 @@
+landmarks = {
+    "Big Ben": 12,
+    "Tower Bridge": 25,
+    "Buckingham Palace": 15,
+    "Madame Tussauds": 25,
+    "London Eye": 40,
+    "Tower of London": 25,
+    "Emirates Air Line cable car": 16,
+    "London Transport Museum": 7,
+    "Wembley Stadium": 8,
+    "Hyde Park": 0,
+    "The View from The Shard": 14
+}
+
+short_places = []
+
+for place in landmarks:
+  if landmarks[place] < 15:
+    short_places.append(place)
+
+print(len(short_places), short_places)
